2ec8bd3c004b33be6c78d88e9a088d61a497a258
morfsradio <morfsradio@gmail.com>
2023-06-25 15:58:30 +0100

    First Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ca2mobdev_sba22078',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
